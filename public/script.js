7521eb4b4b231b475dcd28d763a35831a7c5ac2c
Gulmira Berdalieva <gulmira2410@gmail.com>
2017-11-03 20:08:45 -0700

    works partially, need to handle repeating events

--- public/script.js
+++ public/script.js
@@ -1,45 +1,60 @@
-// <span class="title">"${book.title}"</span> 
-// <span class="author">${book.author}</span>
-// 
-// <span class="year">${book.releaseDate}</span>
-// <img src="${book.image}" class="image" height="150"/>
-
+//can't edit again after editing 
+//can't delete a book after editing
+//can't edit or delete the newly added book 
 $(document).ready(function() {
   //place where retrieved data will be displayed 
-  const $ul = $('.list-group'); 
+  const $ul = $('.list-group');
 
-  //function used when making ajax call to api with get method 
+  //calling api and getting list of books to the page  
   const renderBook = (book) => {
     return $("<li></li>").addClass("list-group-item").html(`
       <button type="button" class="btn btn-info edit">Edit</button>
       <button type="button" class="btn btn-default delete">Delete</button>
       
-      <li class="form-control title">"${book.title}"</li> 
-      <li class="form-control author">${book.author}</li>
-      <li class="form-control releaseDate">${book.releaseDate}</li>
-      
-      <li class="form-control image"><img src="${book.image}" class="image" height="150"/></li>
+      <div class="form-control title">${book.title}</div> 
+      <div class="form-control author">${book.author}</div>
+      <div class="form-control releaseDate">${book.releaseDate}</div>
+      <div class="form-control image"><img src="${book.image}" class="image" height="150"/></div>
       `)
       .data("id", book._id)
   };
 
-  //ajax call with method delete 
+  const addBook = (values) => {
+    return $.ajax({
+        method: "POST",
+        url: "http://mutably.herokuapp.com/books",
+        data: values,
+        dataType: "json"
+      })
+      .then(book => {
+        $ul.prepend(renderBook(book));
+        $("#addItem")[0].reset();
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
   const deleteBook = (bookId) => {
     return $.ajax({
-      method: 'DELETE',
-      url: `http://mutably.herokuapp.com/books/${bookId}`
-    });
+        method: 'DELETE',
+        url: `http://mutably.herokuapp.com/books/${bookId}`
+      })
   };
 
-  //ajax call with method put 
-  const saveBook = (bookId, title, author) => {
+  const saveBook = ({
+    book,
+    bookId
+  }) => {
     return $.ajax({
       method: 'PUT',
-      url: `http://mutably.herokuapp.com/books/${bookId}`
-    })
+      url: `http://mutably.herokuapp.com/books/${bookId}`,
+      data: book,
+      dataType: "json"
+    }) 
   };
 
-  //function used for validating form for adding new item  
+  // validating form for adding new item  
   const validateForm = (values) => {
     const rawErrors = Object.keys(values).map(key => {
       if (!values[key]) {
@@ -51,82 +66,71 @@ $(document).ready(function() {
       return '';
     })
     return _.compact(rawErrors);
+  };
+
+  const createInput = (type, cssClass, val) => {
+    return $("<input/>").attr('type', type).addClass(cssClass).val(val);
+  };
+  
+  /**
+   * Creates new element li
+   * @param  {string} cssClass cssClass to apply to li element 
+   * @param  {string} text     text in li 
+   * @return {jQuery.Element}
+   */
+  const createLi = (cssClass, text) => {
+    return $("<li></li>").addClass(cssClass).text(text);
   }
 
-  //ajax call with method post 
-  const addBook = (values) => {
-    return $.ajax({
-        method: "POST",
-        url: "http://mutably.herokuapp.com/books",
-        data: values,
-        dataType: "json"
+
+  const editInfo = (li) => {
+    const title = li.find('.title');
+    const author = li.find('.author');
+    const releaseDate = li.find('.releaseDate');
+    const imageWrapper = li.find('.image');
+    const image = imageWrapper.find('img');
+
+    const titleText = title.text();
+    const authorText = author.text();
+    const releaseDateText = releaseDate.text();
+    const imageSrc = image.prop('src');
+
+    const titleInput = createInput('text', 'title form-control', titleText);
+    const authorInput = createInput('text', 'author form-control', authorText);
+    const releaseDateInput = createInput('text', 'release-date form-control', releaseDateText);
+    const imageInput = createInput('text', 'image-input form-control', imageSrc);
+
+    title.remove();
+    author.remove();
+    releaseDate.remove();
+    imageWrapper.remove();
+
+    li.append(titleInput, authorInput, releaseDateInput, imageInput);
+  };
+
+
+  const saveInfo = (li) => {
+    const titleInput = li.find('.title');
+    const authorInput = li.find('.author');
+    const releaseDateInput = li.find('.release-date'); //correct later 
+    const imageInput = li.find('.image-input');
+
+    saveBook({
+        book: {
+          title: titleInput.val(),
+          author: authorInput.val(),
+          releaseDate: releaseDateInput.val(),
+          image: imageInput.val()
+        },
+        bookId: li.data("id")
       })
       .then(book => {
-        $ul.prepend(renderBook(book));
-        $("#addItem")[0].reset();
-
+        li.replaceWith(renderBook(book));
       })
       .catch(err => {
         console.log(err);
       })
   };
-
-
-  //functions used when editing book info 
-  const createInput = (type, cssClass, val) => {
-    return $("<input/>").attr('type', type).addClass(cssClass).val(val);
-  };
-
-  // const createSpan = (cssClass, text) => {
-  //   return $("<span></span>").addClass(cssClass).text(text);
-  // };
-
-  const createLi = (cssClass, text) => {
-    return $("<li></li>").addClass(cssClass).text(text);
-  }
-
-  //function used when "Edit" button clicked 
-  const editInfo = (li) => {
-    const title = li.find('.title');
-    const author = li.find('.author');
-    const releaseDate = li.find('.releaseDate');
-    const image = li.find('.image')
-
-    const titleText = title.text();
-    const authorText = author.text();
-    
-    // title.removeAttr("readonly");
-
-    const titleInput = createInput('text', 'title form-control', titleText);
-    const authorInput = createInput('text', 'author form-control', authorText);
-    const releaseDateInput = createInput('date', 'releaseDate form-control'); 
-    // const imageInput = createInput('text', 'image form-control')
-    
-    title.remove();
-    author.remove();
-    releaseDate.remove(); 
-    
-    //can we instead of removing and appending new ones, just make inputs editable?
-    li.append(titleInput, authorInput, releaseDateInput);
-    
-  };
-
-  //function used when "Save" button clicked  
-  //so far inputs are still editable even after clicking Save
-  const saveInfo = (li) => {
-    const titleInput = li.find('.title');
-    const authorInput = li.find('.author');
-    const releaseDateInput = li.find('.releaseDate');
-
-    const titleText = titleInput.val();
-    const authorText = authorInput.val();
-    const releaseDateText = releaseDateInput.val();
-
-    const titleLi = createLi('title list-group-item', titleText);
-    const authorLi = createLi('author list-group-item', authorText);
-    const releaseDateLi = createLi('releaseDate list-group-item', releaseDateText);
-  };
-
 
   //getting books from api 
   $.ajax({
@@ -135,7 +139,6 @@ $(document).ready(function() {
       dataType: "json"
     })
     .then(res => {
-      // console.log(res);
       let content = '';
       content = res.books.map(book => renderBook(book));
       $ul.html(content);
@@ -153,7 +156,7 @@ $(document).ready(function() {
         } else {
           console.log(errors);
         }
-      })
+      });
 
       //handler for "Edit" button 
       $(".btn.edit").click((event) => {
@@ -165,6 +168,7 @@ $(document).ready(function() {
           editInfo(li);
         } else {
           button.text('Edit');
+          saveInfo(li);
         }
       });
 
@@ -173,16 +177,17 @@ $(document).ready(function() {
         const button = $(event.target);
         const li = button.parent();
         const id = li.data("id");
-        deleteBook(id).then(() => {
+        deleteBook(id)
+          .then(() => {
           li.remove();
-        }).catch((err) => {
-          console.log(err);
+        })
+          .catch((err) => {
+            console.log(err);
         });
       })
     })
     .catch(err => {
       $ul.html('Error occured.');
     });
-
 
 }); //end of doc ready
